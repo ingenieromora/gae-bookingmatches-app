a9a47d749756d933015764862cb6fac84271e0d3
tomduhourq <t.duhourq@gmail.com>
2015-10-20 02:01:02 -0300

    Adding authentication plus cache against FB

--- src/main/java/org/utn/edu/ar/model/request/Facebook.java
+++ src/main/java/org/utn/edu/ar/model/request/Facebook.java
@@ -1,7 +1,9 @@
 package org.utn.edu.ar.model.request;
 
+import com.google.appengine.repackaged.org.codehaus.jackson.map.ObjectMapper;
 import org.apache.commons.io.IOUtils;
 import org.utn.edu.ar.Constants;
+import org.utn.edu.ar.model.response.FbIdNameResponse;
 
 import javax.net.ssl.HttpsURLConnection;
 import java.io.IOException;
@@ -14,12 +16,40 @@ import java.net.URL;
  */
 public class Facebook {
 
+  // Jackson mapper to parse the response from fb's validation to a class.
+  private static final ObjectMapper mapper = new ObjectMapper();
+
+  // This could be extracted to Constants, but I dunno.
+  private static final String apiVersion = "v2.4/";
   private static final String graphURL = "https://graph.facebook.com/";
+
+  private static final String validateUserURL = graphURL + apiVersion + "/me?access_token=";
   private static final String getAppTokenURL = graphURL +
                   "oauth/access_token?"                 +
                   "client_id="+     Constants.APP_ID    +
                   "&client_secret="+Constants.APP_SECRET+
                   "&grant_type=client_credentials";
+
+  /** This method is responsible for returning the user of the app a response telling
+   * that he/she has been authenticated and is being kept within a cache holding the pair
+   * Access Token / FbId
+   *
+   * @param rq the ValidateRequest to validate.
+   * @return OK if the user is authenticated, FAILED otherwise.
+   */
+  public static String authenticate(final ValidateRequest rq){
+    URL url;
+    try {
+      url = new URL(validateUserURL + rq.getAccessToken());
+      HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+      InputStream in =conn.getInputStream();
+      FbIdNameResponse response = mapper.readValue(in, FbIdNameResponse.class);
+      if(rq.getFbId().equals(response.getId())) return "OK";
+    } catch (IOException e){
+      e.printStackTrace();
+    }
+    return "FAILED";
+  }
 
   public static String getAppAccessToken(){
     URL url;
