1c7926eca2bba10869f7e2f5c5468c2160d672f5
norchow <nicoorchow@gmail.com>
2015-11-07 14:36:24 -0300

    Fix parse matches information for admin

--- src/main/webapp/controllers/matchesCtrl.js
+++ src/main/webapp/controllers/matchesCtrl.js
@@ -11,18 +11,17 @@ angular.module('bookingMatches')
 
 .controller('MatchesCtrl', function($scope, ngTableParams, MatchService) {
 
-	$scope.matches = [];
-	
-	MatchService.getAll().success(function(data){
-		$scope.matches = data;
-	});
-	
-	
-	 $scope.tableParams = new ngTableParams({},
-      {
+    $scope.matches = [];
+
+    MatchService.getAll().success(function(data){
+        $scope.matches = data.items;
+    });
+
+    $scope.tableParams = new ngTableParams({},
+    {
         total: $scope.matches.length,
         getData: function($defer, params) {
-			$defer.resolve($scope.matches);
+            $defer.resolve($scope.matches);
         }
     });
 });
