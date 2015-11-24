9a0ad75fa4317723723eebe9ae3c2c5e3d47f046
Tomas Gropper <tomasgropper@gmail.com>
2015-11-23 22:53:03 -0300

    comento la notificacion

--- src/main/webapp/controllers/matchDetailCtrl.js
+++ src/main/webapp/controllers/matchDetailCtrl.js
@@ -97,8 +97,8 @@ angular.module('bookingMatches')
                 Notification.success({message: 'La inscripción se realizó exitosamente'});
                 FBService.sendNotification('El jugador '+$scope.user.name+' se ha inscripto a un partido que creaste', $scope.match.createdBy.fbId);
 
-                if ($scope.match.starters.length == $scope.match.playersNeeded)
-                    FBService.sendNotification('Se han completado las inscripciones para el partido '+$scope.match.id, $scope.match.createdBy.fbId);
+//                if ($scope.match.starters.length == $scope.match.playersNeeded)
+//                    FBService.sendNotification('Se han completado las inscripciones para el partido '+$scope.match.id, $scope.match.createdBy.fbId);
 
                 $scope.refreshMatchData();
             })
